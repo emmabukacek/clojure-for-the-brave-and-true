6824fa3a0c30281d476c7ea9e217dff4efef15d0
Emma Bukacek <emma.bukacek@gmail.com>
2018-09-18 13:07:08 -0700

    I clearly don't understand namespaces

---
+++ clojure-noob/src/clojure_noob/functional.clj
@@ -0,0 +1,8 @@
+(ns clojure-noob.functional)
+
+(defn sum
+  ([vals] (sum vals 0))
+  ([vals accumulating-total]
+   (if (empty? vals)
+     accumulating-total
+     (sum (rest vals) (+ (first vals) accumulating-total)))))
